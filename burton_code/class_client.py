7da9e4819bdabeba68752c67464293521c540e84
Calvin Hawkes <calvinhawkes@gmail.com>
2011-11-01 17:39:19 -0400

    added scott's code

---
+++ burton_code/class_client.py
@@ -0,0 +1,41 @@
+#!/usr/bin/python
+import socket
+
+#===============FUNCTIONS=====================#
+def legalResponse(var):
+    if(var == 'load' or \
+       var == 'hedge30yr' or \
+       var == 'yield+100bp' or \
+       var == 'yield-100bp' or \
+       var == 'close'):
+        return True
+    else:
+        return False
+
+
+#==============CODE===========================#
+HOST = socket.gethostname()
+PORT = 0x1234
+MSGSIZE = 8192
+s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+s.connect((HOST, PORT))
+
+done = False;
+#Server doesn't need to handle close
+while(not done):
+    #getting user input
+    var = raw_input("Enter Server Command: ")
+    while(not legalResponse(var)):
+        var = raw_input("Invalid Command Error!\nEnter Server Command: ")
+    if(var == 'close'):
+        done = True
+    else:
+        s.send('Hello, world from a python client!')
+        data = s.recv(MSGSIZE)
+        print 'Received response from the server of:', repr(data)
+
+s.close()
+
+
+
+    
